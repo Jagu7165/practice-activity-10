d1621a103f8206660c2c9eb07ae903347acd1fc5
Jorge Aguinaga <jagu7165@gmail.com>
2021-04-10 21:50:29 -0600

    practice 10

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -7,4 +7,40 @@ import 'bootstrap'
 
 // JavaScript
 
+import {movies} from './movies'
+let featured_movie= document.querySelector('.featured')
+for (let m of movies){
+    let movie_thumb= document.getElementById('m' + m.id)
+    movie_thumb.innerHTML=`
+    <img src="${m.poster}">`
+
+    movie_thumb.onclick = function(){ selectmovie(m)}
+}
+function selectmovie(m){
+    featured_movie.innerHTML=`
+    <img src="${m.poster}" style ="float: left;">
+    <h1>${m.title}</h1>
+    <p>${m.plot}</p>`
+}
+
+function searchmovies(event){
+    if(event){
+        event.preventDefault()
+    }
+    let input = document.querySelector('[type="search"]').value || ""
+    for(let m of movies){
+        let movie_thumb = document.getElementById('m' + m.id)
+        if(m.title.toUpperCase().indexOf(input.toUpperCase())==-1){
+            movie_thumb.classList.add('hidden')
+        }
+        else{
+            movie_thumb.classList.remove('hidden')
+
+        }
+    }
+}
+
+document.querySelector('button').onclick = searchmovies
+document.querySelector('[type="search"]').onsearch = searchmovies
+document.forms[0].addEventListener('submit', searchmovies, false)
 //TODO
